7f7e5930f64092ec3edcae45ce3079bb2037105d
KarlLuther <ivanparapanov1337@gmail.com>
2023-03-31 09:40:59 +0100

    a

--- example.sql
+++ example.sql
@@ -1,14 +1,29 @@
 \c nc_games_test
 
 
-SELECT * FROM comments;
 
 
-DELETE FROM comments WHERE comment_id = 1;
+-- get review by id
 
-SELECT * FROM comments;
+-- SELECT * FROM reviews
+--   WHERE review_id = $1; 
 
-DELETE FROM comments WHERE comment_id = 2;
-SELECT * FROM comments;
 
--- DELETE FROM table_name WHERE condition;
+-- get reviews with comments
+
+SELECT 
+      reviews.title,
+  reviews.designer,
+  reviews.owner,
+  reviews.review_img_url,
+  reviews.category,
+  reviews.created_at,
+  reviews.votes,
+  reviews.review_id,
+  COUNT(comments.comment_id) AS comment_count
+      FROM reviews
+      LEFT JOIN comments
+      ON reviews.review_id = comments.review_id
+      WHERE review.review_id = 1
+      GROUP BY reviews.review_id
+      ORDER BY created_at DESC;
