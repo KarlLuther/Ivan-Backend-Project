77f4b194a0679af85567aea1cc3bd7c6f39883bc
KarlLuther <ivanparapanov1337@gmail.com>
2023-04-12 14:21:35 +0100

    please

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -505,3 +505,15 @@ describe("GET /api/users", () => {
       });
   });
 });
+
+describe.only("GET /api", () => {
+  test("should respond with a JSON describing all the available endpoint on the API", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        const endpoints = body;
+        expect(endpoints.length).toBe(8);
+      });
+  });
+});
